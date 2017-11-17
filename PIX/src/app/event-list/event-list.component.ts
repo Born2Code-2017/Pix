59aa79fbf2e5a58188b8d0dfa2f5bad601e8cd34
valewired <32166580+valewired@users.noreply.github.com>
2017-11-17 15:26:09 +0100

    event-list.ts
    Create event-list.ts

--- PIX/src/app/event-list/event-list.component.ts
+++ PIX/src/app/event-list/event-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Event } from './events/events.model';
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventListComponent implements OnInit {
 
-  constructor() { }
+  eventsList: Event[];
+
+  constructor() {
+    this.loadEventList();
+  }
 
   ngOnInit() {
+  }
+
+  loadEventList() {
+    this.eventsList = [
+      {
+        id: 0,
+        name: "astrophotography",
+        description: "Sto cazzo",
+        date: "12/12/2017",
+        hour: "6:30 pm",
+        owner: "Robert Foster ",
+        location: "Monte Mario,Roma",
+        image: "",
+        partecipants: 57
+      },
+      {
+        id: 1,
+        name: "Portrait",
+        description: "Sto cazzo",
+        date: "13/12/2017",
+        hour: "4:30 pm",
+        owner: "Ilaria Vercelli ",
+        location: "Milano,Ilaria's House",
+        image: "./img/img4.jpg",
+        partecipants:37
+      }
+    ]
   }
 
 }
