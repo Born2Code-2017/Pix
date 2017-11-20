0672f9b90240e6192ef87213a18bc8883d57b896
matteo-g <matteo1.galdi@gmail.com>
2017-11-20 15:51:28 +0100

    Add <app.service>
    Add <app.service> to perform an http request to our DB. Enjoy!

---
+++ PIX/src/app/app.service.ts
@@ -0,0 +1,31 @@
+import {Event} from "./events/events.model";
+import {Injectable} from "@angular/core";
+import {Http, Response} from "@angular/http";
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class PixService {
+  private dbUrl: string;
+
+  constructor(private http: Http) {
+    this.dbUrl = 'https://pics-313d5.firebaseio.com/eventi/';
+  }
+
+  public getEvents() {
+    return this.http.get(this.dbUrl)
+      .map((response: Response) => response.json());
+  }
+
+  public createEvents() {
+    return this.http.post(this.dbUrl, Event)
+      .map((response: Response) => response.json());
+  }
+
+  public deleteEvent() {
+    return this.http.delete(this.dbUrl)
+      .map((response: Response) => response.json());
+  }
+
+}
+
+
